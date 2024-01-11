0a7f95079fe322ef53a5fba4f93706754e5dd7aa
Neha Ghaty <nghatysa@uci.edu>
2024-01-11 13:48:17 -0800

    open file

--- src/pfm/pfm.cc
+++ src/pfm/pfm.cc
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <filesystem>
 #include <iostream>
+#include <sys/stat.h>
 #include "src/include/pfm.h"
 
 namespace PeterDB {
@@ -17,9 +18,16 @@ namespace PeterDB {
 
     PagedFileManager &PagedFileManager::operator=(const PagedFileManager &) = default;
 
+    // Check whether the given file exists
+    bool fileExists(const std::string &fileName) {
+        struct stat stFileInfo{};
+
+        return stat(fileName.c_str(), &stFileInfo) == 0;
+    }
+
     RC PagedFileManager::createFile(const std::string &fileName) {
-        if (std::filesystem::exists(fileName.c_str())){
-            std::cout << "file already exists" << std::endl;
+        if (fileExists(fileName)){
+            std::cout << "File already exists" << std::endl;
             return -1;
         }
         else{
@@ -27,11 +35,11 @@ namespace PeterDB {
             std::fstream file(fileName.c_str(), std::ios::out | std::ios::in |
             std::ios::trunc|std::ios::binary);
             if(file){
-                std::cout << "file created successfully" << std::endl;
+                std::cout << "File created successfully" << std::endl;
                 return 0;
             }
         }
-        std::cout << "file creation failed" << std::endl;
+        std::cout << "File creation failed" << std::endl;
         return -1;
     }
 
@@ -40,7 +48,24 @@ namespace PeterDB {
     }
 
     RC PagedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
-        return -1;
+        // check if file exists
+        if(fileExists(fileName)){
+            std::fstream file;
+            file.open(fileName.c_str(), std::ios::out|std::ios::in|std::ios::app);
+            if (!file) {
+                std::cout << "File not opened"<< std::endl;
+                return -1;
+            }
+            else {
+                std::cout << "File opened successfully" << std::endl;
+                fileHandle = FileHandle();
+                return 0;
+            }
+        }
+        else{
+            std::cout << "File does not exist" << std::endl;
+            return -1;
+        }
     }
 
     RC PagedFileManager::closeFile(FileHandle &fileHandle) {
