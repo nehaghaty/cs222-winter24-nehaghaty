da0a99c6e013a6f0e9a9a7311666c290a18747e1
Sanket <sankuhs99@gmail.com>
2024-01-13 19:21:43 -0800

    Write, Append & helper function changes

--- src/pfm/pfm.cc
+++ src/pfm/pfm.cc
@@ -25,6 +25,15 @@ namespace PeterDB {
         return stat(fileName.c_str(), &stFileInfo) == 0;
     }
 
+    static long getFileSize(FILE *file) {
+        long size;
+        fseek(file, 0, SEEK_END);
+
+        size = ftell(file);
+
+        return size;
+    }
+
     RC PagedFileManager::createFile(const std::string &fileName) {
         if (fileExists(fileName)){
             std::cout << "File already exists" << std::endl;
@@ -32,10 +41,10 @@ namespace PeterDB {
         }
         else{
             //create file
-            std::fstream file(fileName.c_str(), std::ios::out | std::ios::in |
-            std::ios::trunc);
+            FILE *file = fopen(fileName.c_str(), "wb");
             if(file){
-                std::cout << "File created successfully" << std::endl;
+                //std::cout << "File created successfully" << std::endl;
+                fclose(file);
                 return 0;
             }
         }
@@ -44,74 +53,128 @@ namespace PeterDB {
     }
 
     RC PagedFileManager::destroyFile(const std::string &fileName) {
-        return -1;
+        if (fileExists(fileName.c_str())){
+            //destroy file
+            std::remove (fileName.c_str());
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     RC PagedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
         // check if file exists
-        if(fileExists(fileName)){
+        if (fileExists(fileName)){
             FILE *file;
-            file = fopen(fileName.c_str(), "wb");
+            file = fopen(fileName.c_str(), "r+b");
             if (!file) {
                 std::cout << "File not opened" << std::endl;
                 return -1;
             }
             else {
-                std::cout << "File opened successfully" << std::endl;
+                //std::cout << "File opened successfully" << std::endl;
                 if(fileHandle.opened)
                 {
                     std::cout<<"File handle in use!" << std::endl;
                     return -1;
                 }
-                else{
+                else {
+
                     fileHandle.opened=true;
                     fileHandle.file = file;
-                    //create hidden page
-                    void *hiddenPage = malloc(PAGE_SIZE);
-                    memset(hiddenPage,0,PAGE_SIZE);
-                    fwrite(hiddenPage, PAGE_SIZE, 1, file);
-                    std::cout<<"Hidden file created!" << std::endl;
+                    //check for hidden page, create only if not present already
+                    if (getFileSize(file) == 0) {
+                        void *hiddenPage = malloc(PAGE_SIZE);
+                        memset(hiddenPage, 0, PAGE_SIZE);
+                        std::fwrite(hiddenPage, PAGE_SIZE, 1, file);
+                        free(hiddenPage);
+                    } else {
+                        fseek(file, 0, SEEK_SET);
+                        fread(&fileHandle.totalPages, sizeof (int), 1, file);
+                        //std::cout << "Total Pages: " <<fileHandle.totalPages << std::endl;
+                    }
+//                    std::cout<<"Hidden file created!" << std::endl;
                     return 0;
                 }
 
             }
         }
-        else{
+        else {
             std::cout << "File does not exist" << std::endl;
             return -1;
         }
     }
 
     RC PagedFileManager::closeFile(FileHandle &fileHandle) {
-        return -1;
+        if (fileHandle.opened) {
+            fileHandle.opened = false;
+            std::fseek(fileHandle.file, 0, SEEK_SET);
+            std::fwrite(&fileHandle.totalPages, sizeof (int), 1, fileHandle.file);
+            fclose(fileHandle.file);
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     FileHandle::FileHandle() {
         readPageCounter = 0;
         writePageCounter = 0;
         appendPageCounter = 0;
+        totalPages = 0;
     }
 
     FileHandle::~FileHandle() = default;
 
     RC FileHandle::readPage(PageNum pageNum, void *data) {
-        return -1;
+        if (pageNum <= totalPages) {
+            fseek(file, (pageNum + 1) * PAGE_SIZE, SEEK_SET);
+            fread(data, PAGE_SIZE, 1, file);
+            readPageCounter++;
+            return 0;
+        }
+        else {
+            return -1;
+        }
     }
 
     RC FileHandle::writePage(PageNum pageNum, const void *data) {
-        return -1;
+
+        if (pageNum > totalPages) {
+            return -1;
+        }
+
+        if (!fseek(file, (pageNum + 1) * PAGE_SIZE, SEEK_SET)) {
+            fwrite(data, PAGE_SIZE, 1, file);
+            writePageCounter+=1;
+            return 0;
+        }
+        else {
+            std::cout << "Error writing data to particular page" << std::endl;
+            return -1;
+        }
     }
 
     RC FileHandle::appendPage(const void *data) {
-        return -1;
+        fseek(file, (totalPages + 1) * PAGE_SIZE, SEEK_SET);
+        fwrite(data, PAGE_SIZE, 1, file);
+        appendPageCounter++;
+        totalPages++;
+        return 0;
     }
 
     unsigned FileHandle::getNumberOfPages() {
-        return -1;
+        return totalPages;
     }
 
     RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
-        return -1;
+        readPageCount = readPageCounter;
+        writePageCount = writePageCounter;
+        appendPageCount = appendPageCounter;
+
+        return 0;
     }
 
 } // namespace PeterDB
